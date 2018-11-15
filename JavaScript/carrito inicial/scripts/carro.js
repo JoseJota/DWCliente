849faec8496516b906673e1da9b442b6f121c144
JoseJota <jose.julian.perez.lucas@gmail.com>
2018-11-15 12:49:29 +0100

    Carrito

---
+++ JavaScript/carrito inicial/scripts/carro.js
@@ -0,0 +1,7 @@
+$(function() {
+  $(".item").css("background-color", "#cecece");
+  $("#cart_items").css("border", "solid blue 1px");
+  $(".item > label").css("text-decoration", "underline");
+  $(".cart_container button").css("color", "red");
+  $("").css("color", "green");
+});
